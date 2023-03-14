a3041dd613ce6fec14730079ce3b18f00f54ecb3
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-03-14 10:37:00 -0300

    aula 68: useRef #01

--- src/views/examples/UseRef.jsx
+++ src/views/examples/UseRef.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useRef, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
 const UseRef = (props) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+  const [value1, setValue] = useState("");
+  const count = useRef(0);
 
-export default UseRef
+  useEffect(function() {
+    count.current = count.current + 1;
+  }, [value1]);
+
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercício #01" />
+      <div className="center">
+        <div>
+          <span className="text">Valor: </span>
+          <span className="text">{value1} [</span>
+          <span className="text red">{count.current}</span>
+          <span className="text">]</span>
+        </div>
+        <input
+          type="text"
+          className="input"
+          value={value1}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default UseRef;
